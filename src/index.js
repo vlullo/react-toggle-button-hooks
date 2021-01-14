2ff90f041798f0acb6380c00e17da459da9feb62
Valentina Lullo <valentinalullo88@gmail.com>
2021-01-14 00:43:11 +0000

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+
 const Toggle = () => {
   const [on, setOn] = useState(true);
 
   return (
     <button onClick={() => setOn(!on)}>{ on ? 'ON' : 'OFF' }</button>
   );
-}
+};
 
 ReactDOM.render(
     <Toggle />,
   document.getElementById('root')
 );
-
 
